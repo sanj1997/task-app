f7671d63ed8dc50da1d278ba15cee3e2bb48b3d3
Js Sanjay <mname8741@gmail.com>
2023-01-13 09:28:27 +0530

    bug-fixed

--- task-scheduler/config/dbConnect.js
+++ task-scheduler/config/dbConnect.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
-
 const dbConnect=()=>{
-    return mongoose.connect(process.env.MONGODB_URL)
+    return mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URL)
 }
 
 module.exports=dbConnect
